14e829d2f58093fcf90248779a231a205a8fe638
Dieter Pisarewski <dieter.pisarewski@arvatosystems.com>
2012-05-04 11:20:06 -0400

    Fixed checking rails version

--- lib/crowd_authentication.rb
+++ lib/crowd_authentication.rb
@@ -1,2 +1,2 @@
 require "crowd_authentication/controller"
-require 'crowd_authentication/railtie' if defined? Rails
+require 'crowd_authentication/railtie' if defined? Rails and Rails.version > "3"
--- lib/crowd_authentication/version.rb
+++ lib/crowd_authentication/version.rb
@@ -1,3 +1,3 @@
 module CrowdAuthentication
-  VERSION = "0.3.1"
+  VERSION = "0.3.2"
 end
